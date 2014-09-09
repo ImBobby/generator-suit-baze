67adfd3eff6fa6d08b5039cfa8b3058ec0ebcc69
ImBobby <squidz.bee@gmail.com>
2014-09-09 19:59:07 +0700

    initial commit

---
+++ generators/app/index.js
@@ -0,0 +1,97 @@
+var yeoman  = require('yeoman-generator'),
+    file    = require('yeoman-generator').file;
+
+// Path config
+var endPath = '_front-end/';
+var Path = {
+
+    sass: {
+        root    : 'dev/sass/',
+        mixin   : 'dev/sass/mixin/',
+        modules : 'dev/sass/modules/',
+        partial : 'dev/sass/partial/',
+        plugin  : 'dev/sass/plugin/'
+    },
+
+    js: {
+        root    : 'dev/js/',
+        vendor  : 'dev/js/vendor/'
+    },
+
+    img: {
+        root    : 'dev/img/',
+        webp    : 'dev/img/webp/'
+    }
+
+};
+
+var SuitBaze = module.exports = yeoman.generators.Base.extend({
+
+    default: {
+
+        copyFiles: function () {
+
+            // .gitignore
+            this.src.copy('_gitignore'  , '.gitignore');
+
+            // Copy files
+            this.src.copy('home.html'   , 'home.html');
+            this.src.copy('gulpfile.js' , 'gulpfile.js');
+            this.src.copy('package.json', 'package.json');
+            this.src.copy('bower.json'  , 'bower.json');
+
+            // ---------- Copy SCSS Files ---------- //
+            // main
+            this.src.copy( Path.sass.root + 'main.scss'             , Path.sass.root + 'main.scss' );
+
+            // mixins
+            this.src.copy( Path.sass.mixin + '_mixin.scss'          , Path.sass.mixin + '_mixin.scss' );
+            this.src.copy( Path.sass.mixin + '_mq.scss'             , Path.sass.mixin + '_mq.scss' );
+
+            // modules
+            this.src.copy( Path.sass.modules + '_baze-grid.scss'    , Path.sass.modules + '_baze-grid.scss' );
+            this.src.copy( Path.sass.modules + '_breadcrumb.scss'   , Path.sass.modules + '_breadcrumb.scss' );
+            this.src.copy( Path.sass.modules + '_buttons.scss'      , Path.sass.modules + '_buttons.scss' );
+            this.src.copy( Path.sass.modules + '_forms.scss'        , Path.sass.modules + '_forms.scss' );
+            this.src.copy( Path.sass.modules + '_modules.scss'      , Path.sass.modules + '_modules.scss' );
+            this.src.copy( Path.sass.modules + '_table.scss'        , Path.sass.modules + '_table.scss' );
+            this.src.copy( Path.sass.modules + '_typography.scss'   , Path.sass.modules + '_typography.scss' );
+
+            // partial
+            this.src.copy( Path.sass.partial + '_helpers.scss'      , Path.sass.partial + '_helpers.scss' );
+            this.src.copy( Path.sass.partial + '_normalize.scss'    , Path.sass.partial + '_normalize.scss' );
+            this.src.copy( Path.sass.partial + '_print.scss'        , Path.sass.partial + '_print.scss' );
+            this.src.copy( Path.sass.partial + '_resets.scss'       , Path.sass.partial + '_resets.scss' );
+            this.src.copy( Path.sass.partial + '_variables.scss'    , Path.sass.partial + '_variables.scss' );
+
+            // plugin
+            this.src.copy( Path.sass.plugin + '_plugin.scss'        , Path.sass.plugin + '_plugin.scss' );
+
+
+            // ---------- Copy JS Files ---------- //
+            // main
+            this.src.copy( Path.js.root + 'main.js'         , Path.js.root + 'main.js' );
+
+            // vendor
+            this.src.copy( Path.js.vendor + 'fastclick.js'  , Path.js.vendor + 'fastclick.js' );
+            this.src.copy( Path.js.vendor + 'jquery.js'     , Path.js.vendor + 'jquery.js' );
+            this.src.copy( Path.js.vendor + 'modernizr.js'  , Path.js.vendor + 'modernizr.js' );
+
+
+            // ---------- Copy images Files ---------- //
+            this.src.copy( Path.img.root + 'apple-icon.png' , Path.img.root + 'apple-icon.png' );
+            this.src.copy( Path.img.root + 'favicon.png'    , Path.img.root + 'favicon.png' );
+        }
+        
+    },
+
+    install: {
+
+        installDep: function () {
+            this.installDependencies();
+        }
+
+    }
+
+
+});
---
+++ generators/app/templates/dev/js/main.js
@@ -0,0 +1,70 @@
+/*******************************************************************************
+
+    Title :  
+    Date  :  September 2014
+    Author:  Suitmedia (http://www.suitmedia.com)
+
+********************************************************************************/
+
+var Site = {
+
+    assets: {
+        _jquery_cdn     : '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
+        _jquery_local   : myPrefix + 'assets/js/vendor/jquery.min.js',
+        _fastclick      : myPrefix + 'assets/js/vendor/fastclick.min.js'
+    },
+
+    init: function () {
+        Site.fastClick();
+        Site.enableActiveStateMobile();
+        Site.WPViewportFix();
+    },
+
+    fastClick: function () {
+        Modernizr.load({
+            load    : Site.assets._fastclick,
+            complete: function () {
+                FastClick.attach(document.body);
+            }
+        });
+    },
+
+    enableActiveStateMobile: function () {
+        if ( document.addEventListener ) {
+            document.addEventListener('touchstart', function () {}, true);
+        }
+    },
+
+    WPViewportFix: function () {
+        if ( navigator.userAgent.match(/IEMobile\/10\.0/) ) {
+            var style   = document.createElement("style"),
+                fix     = document.createTextNode("@-ms-viewport{width:auto!important}");
+
+            style.appendChild(fix);
+            document.getElementsByTagName('head')[0].appendChild(style);
+        }
+    }
+
+};
+
+(function () {
+    var siteInit = function () {
+        Site.init();
+    };
+
+    var checkJquery = function () {
+        if ( !window.jQuery ) {
+            Modernizr.load({
+                load    : Site.assets._jquery_local,
+                complete: siteInit
+            });
+        } else {
+            Site.init();
+        }
+    };
+
+    Modernizr.load({
+        load    : Site.assets._jquery_cdn,
+        complete: checkJquery
+    });
+})();
